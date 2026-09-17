ea270fae30820ce431c3fd069dd676cf92e15251
zack <isaacpechherrera@gmail.com>
2026-09-17 01:07:51 -0600

    Add initial project structure and configuration for BookingService API
    - Create .gitignore to exclude unnecessary files
    - Add solution file for BookingService
    - Implement BookingService.Api project with basic setup
    - Add launch settings for local development
    - Include appsettings for logging configuration
    - Establish CORS policy and Swagger integration

---
+++ README.md
@@ -0,0 +1,12 @@
+# BookingService (sap-atitos)
+- {MVP - 02}
+
+Backend responsible for the purchase process and ticket issuance
+
+## Requirements
+- .NET 8.0+ SDK
+
+## How to run locally
+<code>
+dotnet run --project src/BookingService.Api
+</code>
---
+++ src/BookingService.Api/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.MapGet("/", () => Results.Ok(new { status = "BookingService API Online", version = "0.0.1"}));
+
+app.Run();
