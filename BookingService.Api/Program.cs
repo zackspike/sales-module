bf245a0994c7dc44c0c57abbc5a68d2e9e08b481
zack <isaacpechherrera@gmail.com>
2026-09-24 22:15:05 -0600

    refactor: clean up unused using directives in Program.cs

--- BookingService.Api/Program.cs
+++ BookingService.Api/Program.cs
@@ -1,14 +1,7 @@
 using BookingService.Api.Common;
 using BookingService.Api.Endpoints;
 using BookingService.Infrastructure;
-<<<<<<< HEAD
 using Microsoft.OpenApi;
-=======
-using BookingService.Infrastructure.Repositories;
-using BookingService.Application.Repositories;
-using BookingService.Application.Tickets;
-using BookingService.Api.Common;
->>>>>>> 82c24c8eb6b9c9271270f9077dbe8612fff2f605
 
 var builder = WebApplication.CreateBuilder(args);
 
